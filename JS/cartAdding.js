ddb548181d06c7bffc387abc41df26c6a29ce941
Markus Katen <Markus.Katen478@west-mec.org>
2017-05-19 18:10:40 -0700

    thursday commit

---
+++ JS/cartAdding.js
@@ -0,0 +1,8 @@
+function addToCart(id) {
+    var x = document.getElementById('item1');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
