73dbd7569f7696a6d4cb4f7aff2403dd66718086
Markus <Markus.katen478@west-mec.org>
2017-05-21 14:21:18 -1200

    sunday comentting and attempt to use local storage commit

--- JS/cartAdding.js
+++ JS/cartAdding.js
@@ -1,8 +1,29 @@
-function addToCart(id) {
-    var x = document.getElementById('item1');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
+// Declaring Array
+var itemsArray = [""];
+// Turning array into string so it can be worked with local storage
+JSON.stringify(itemsArray);
+
+// Function that puts the id of the button that you clicked on the shopping page inside the array
+function addToCart(itemNumber) {
+  // alert(itemNumber);
+  localStorage.setItem(itemNumber, JSON.stringify(itemsArray));
+  // alert(JSON.stringify(itemsArray));
+}
+// When checkout page is loaded Generate page function is ran
+if (window.location.href.match('checkout.html') != null) {
+  generatePage();
+}
+
+// Function that gets the strings out of the arrays and displays the corresponding item div 
+function generatePage() {
+  // alert("linked");
+  for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+    // alert( localStorage.getItem( localStorage.key( i ) ) );
+  };
+  document.getElementById("result").innerHTML = items;
+  if (items.style.display === 'none') {
+      items.style.display = 'block';
+  } else {
+      items.style.display = 'none';
+  }
 }
